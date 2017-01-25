3b57a75e75f736a4473faaf260dd831c1d5932a2
Petr Doležal <petr.dolezal@matfyz.cz>
2017-01-25 15:58:09 +0100

    Add Types::cast

--- src/main/java/net/yetamine/lang/Types.java
+++ src/main/java/net/yetamine/lang/Types.java
@@ -36,6 +36,37 @@ public final class Types {
         return (Class<? extends T>) object.getClass();
     }
 
+    /**
+     * Casts a {@link Class} instance to match its actual generic argument to
+     * the desired type, which is often the result of the type inference when
+     * compiling the code.
+     *
+     * <p>
+     * The cast is unchecked and can't be checked in runtime, therefore the user
+     * takes the responsibility for the correctness of such a cast. However, the
+     * cast is useful for generics which are non-reifiable and such a cast can't
+     * be avoided.
+     *
+     * <p>
+     * A typical and harmless example of using this method is for casting from
+     * raw to a generic reifiable type:
+     *
+     * <pre>
+     * final Class&lt;Set&lt;?&gt;&gt; set = Types.cast(HashSet.class);
+     * </pre>
+     *
+     * @param <T>
+     *            the type of resulting values
+     * @param clazz
+     *            the class to cast to
+     *
+     * @return the given argument cast to the desired formal type
+     */
+    @SuppressWarnings("unchecked")
+    public static <T> Class<T> cast(Class<?> clazz) {
+        return (Class<T>) clazz;
+    }
+
     private Types() {
         throw new AssertionError();
     }
