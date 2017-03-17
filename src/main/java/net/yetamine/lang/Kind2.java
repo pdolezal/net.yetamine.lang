b065897951b9679cad85826c729b24707e58aabb
Petr Doležal <petr.dolezal@matfyz.cz>
2017-03-17 10:46:13 +0100

    Add Kind1 and Kind2

---
+++ src/main/java/net/yetamine/lang/Kind2.java
@@ -0,0 +1,32 @@
+/*
+ * Copyright 2017 Yetamine
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package net.yetamine.lang;
+
+/**
+ * Recursive self type representing {@code K&lt;T&gt;}, which allows similar
+ * behavior to higher-kinded types.
+ *
+ * @param <K>
+ *            the recursive kind type
+ * @param <T1>
+ *            the first component type of the kind type
+ * @param <T2>
+ *            the second component type of the kind type
+ */
+public interface Kind2<K extends Kind2<K, ?, ?>, T1, T2> {
+    // Type-defining interface
+}
